e92cff938cc9b3484848fdeb4d82d379bf19cb07
DamjanT1988 <Damjan_t@hotmail.com>
2023-02-26 09:11:15 +0100

    Add project files.

---
+++ 191-PROJECT-BACKEND/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using _191_PROJECT_BACKEND.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _191_PROJECT_BACKEND.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 191-PROJECT-BACKEND/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _191_PROJECT_BACKEND.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
