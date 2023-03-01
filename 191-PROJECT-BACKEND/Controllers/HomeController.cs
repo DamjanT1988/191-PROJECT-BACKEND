adafec9b4f00c249593bb72e4ec5bce464e14c68
DamjanT1988 <Damjan_t@hotmail.com>
2023-03-01 13:50:09 +0100

    1

--- 191-PROJECT-BACKEND/Controllers/HomeController.cs
+++ 191-PROJECT-BACKEND/Controllers/HomeController.cs
@@ -58,6 +58,34 @@ namespace _191_PROJECT_BACKEND.Controllers
         {
             if (ModelState.IsValid)
             {
+                //image or not?
+                if (productModel.Image_file != null)
+                {
+                    //save image to wwwroot/imageupload
+                    string fileName = Path.GetFileNameWithoutExtension(productModel.Image_file.FileName);
+                    string extension = Path.GetExtension(productModel.Image_file.FileName);
+
+                    //remove space and add time
+                    productModel.Image_path = fileName = fileName.Replace(" ", String.Empty) + DateTime.Now.ToString("yymmssfff") + extension;
+
+                    //path
+                    string path = Path.Combine("wwwroot/imageupload/", fileName);
+
+                    //store file
+                    using (var fileStream = new FileStream(path, FileMode.Create))
+                    {
+                        await productModel.Image_file.CopyToAsync(fileStream);
+                    }
+
+                    //create miniatures
+                    //CreateImageFiles(fileName);
+
+                }
+                else
+                {
+                    productModel.Image_path = null;
+                }
+
                 _context.Add(productModel);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
